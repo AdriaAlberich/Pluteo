f4886ccd86426f30c6782bb6aa50f2285a3ee3ad
Adrià Alberich Jaume <alberichjaumeadria@gmail.com>
2025-04-07 20:13:16 +0200

    feature-pluteo-notification-controller: added notification controller

--- backend/PluteoDomain/Interfaces/Systems/INotificationSystem.cs
+++ backend/PluteoDomain/Interfaces/Systems/INotificationSystem.cs
@@ -5,6 +5,11 @@ public interface INotificationSystem
 {
     Task AddNotification(User user, string title, string content, bool markAsRead = false);
     Task<List<Notification>> GetNotificationList(User user);
-    Task RemoveNotification(User character, Guid notificationId);
-    Task NotificationClick(User character, Guid notificationId);
+    Task RemoveNotification(User user, Guid notificationId);
+    Task NotificationClick(User user, Guid notificationId);
+    Task SortNotifications(User user);
+    Task<List<Notification>> GetUserNotifications(string email);
+    Task NotificationUserClick(string email, Guid notificationId);
+    Task RemoveUserNotification(string email, Guid notificationId);
+    Task RemoveUserNotifications(string email);
 }
---
+++ backend/PluteoInfrastructure/Controllers/NotificationController.cs
@@ -0,0 +1,137 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
+using ILogger = Serilog.ILogger;
+using Pluteo.Domain.Exceptions;
+using Pluteo.Infrastructure.Utils;
+using Pluteo.Domain.Models.Dto.Users;
+using Pluteo.Domain.Models.Settings;
+using System.Security.Claims;
+using Pluteo.Application.Systems;
+
+namespace Pluteo.Infrastructure.Controllers;
+[ApiController]
+[Produces("application/json")]
+[Route("api/notifications")]
+public class NotificationController(NotificationSystem notificationSystem, IWebHostEnvironment env, ILogger logger, IMapper mapper, ApplicationSettings config) : Controller
+{
+    private readonly NotificationSystem _notificationSystem = notificationSystem;
+    private readonly ILogger _logger = logger;
+    private readonly IWebHostEnvironment _env = env;
+    private readonly IMapper _mapper = mapper;
+    private readonly ApplicationSettings _config = config;
+
+    #region User Endpoints
+
+    [Authorize(Roles = "User")]
+    [HttpGet()]
+    public async Task<ActionResult> GetUserNotifications()
+    {
+        try
+        {
+            var userEmail = GetUserEmail(User);
+
+            if (string.IsNullOrWhiteSpace(userEmail))
+                return BadRequest("USER_EMAIL_NULL");
+
+            return Ok(await _notificationSystem.GetUserNotifications(userEmail));
+        }
+        catch (ServiceException se)
+        {
+            return StatusCode(StatusCodes.Status406NotAcceptable, ExceptionControl.ProcessException(se, _logger, _env.IsDevelopment(), true));
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ExceptionControl.ProcessException(e, _logger, _env.IsDevelopment(), false));
+        }
+    }
+
+    [Authorize(Roles = "User")]
+    [HttpPatch("{notificationId}")]
+    public async Task<ActionResult> NotificationUserClick(Guid notificationId)
+    {
+        try
+        {
+            var userEmail = GetUserEmail(User);
+
+            if (string.IsNullOrWhiteSpace(userEmail))
+                return BadRequest("USER_EMAIL_NULL");
+
+            await _notificationSystem.NotificationUserClick(userEmail, notificationId);
+
+            return Ok();
+        }
+        catch (ServiceException se)
+        {
+            return StatusCode(StatusCodes.Status406NotAcceptable, ExceptionControl.ProcessException(se, _logger, _env.IsDevelopment(), true));
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ExceptionControl.ProcessException(e, _logger, _env.IsDevelopment(), false));
+        }
+    }
+
+    [Authorize(Roles = "User")]
+    [HttpDelete("{notificationId}")]
+    public async Task<ActionResult> RemoveUserNotification(Guid notificationId)
+    {
+        try
+        {
+            var userEmail = GetUserEmail(User);
+
+            if (string.IsNullOrWhiteSpace(userEmail))
+                return BadRequest("USER_EMAIL_NULL");
+
+            await _notificationSystem.RemoveUserNotification(userEmail, notificationId);
+
+            return Ok();
+        }
+        catch (ServiceException se)
+        {
+            return StatusCode(StatusCodes.Status406NotAcceptable, ExceptionControl.ProcessException(se, _logger, _env.IsDevelopment(), true));
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ExceptionControl.ProcessException(e, _logger, _env.IsDevelopment(), false));
+        }
+    }
+
+    [Authorize(Roles = "User")]
+    [HttpDelete()]
+    public async Task<ActionResult> RemoveUserNotifications()
+    {
+        try
+        {
+            var userEmail = GetUserEmail(User);
+
+            if (string.IsNullOrWhiteSpace(userEmail))
+                return BadRequest("USER_EMAIL_NULL");
+
+            await _notificationSystem.RemoveUserNotifications(userEmail);
+
+            return Ok();
+        }
+        catch (ServiceException se)
+        {
+            return StatusCode(StatusCodes.Status406NotAcceptable, ExceptionControl.ProcessException(se, _logger, _env.IsDevelopment(), true));
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, ExceptionControl.ProcessException(e, _logger, _env.IsDevelopment(), false));
+        }
+    }
+    
+    #endregion
+
+    #region Private methods
+
+        private static string? GetUserEmail(ClaimsPrincipal? user)
+        {
+            ArgumentNullException.ThrowIfNull(user);
+
+            var emailClaim = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+            return emailClaim?.Value;
+        }
+
+        #endregion
+}
