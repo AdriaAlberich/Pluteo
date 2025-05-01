9ccb0b227cdf34fd8428c7472bc8c8f222158cec
Adrià Alberich Jaume <alberichjaumeadria@gmail.com>
2025-05-01 16:31:41 +0200

    features/feature-pluteo-frontend-library: added library related data

--- frontend/src/context/appStore.ts
+++ frontend/src/context/appStore.ts
@@ -8,6 +8,8 @@ interface AppState {
   setNotifications: (notifications: any[]) => void;
   userSettings: any;
   setUserSettings: (settings: any) => void;
+  library: any;
+  setLibrary: (library: any) => void;
 }
 
 export interface UserSettings {
@@ -16,6 +18,22 @@ export interface UserSettings {
   notifyLoanBeforeDays: number;
   notifyLoanBeforeDaysFrequency: number;
   locale: string;
+}
+
+export interface ShelfBook {
+  id: string;
+  title: string;
+  isbn: string;
+  cover: string;
+  authors: string;
+  publisher: string;
+  publishPlace: string;
+  firstPublishYear: string;
+  numPages: string;
+  availableLanguages: string;
+  notes: string;
+  physicalLocation: string;
+  status: string;
 }
 
 export const useAppStore = create<AppState>((set) => ({
@@ -29,5 +47,7 @@ export const useAppStore = create<AppState>((set) => ({
     if (settings && settings.locale) {
       i18n.changeLanguage(settings.locale);
     }
-  }
+  },
+  library: {},
+  setLibrary: (library) => set({ library }),
 }));
